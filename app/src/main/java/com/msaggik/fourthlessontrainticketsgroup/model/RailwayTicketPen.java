93a03513861ccf3a1f0151cb8de86ac7842ee888
ПК <artyom.obrosov@yandex.ru>
2023-02-11 20:21:46 +0300

    Initial commit

---
+++ app/src/main/java/com/msaggik/fourthlessontrainticketsgroup/model/RailwayTicketPen.java
@@ -0,0 +1,21 @@
+package com.msaggik.fourthlessontrainticketsgroup.model;
+import com.msaggik.fourthlessontrainticketsgroup.model.RailwayTicket;
+
+public class RailwayTicketPen extends RailwayTicket {
+    // задание полей
+    private float ticketDiscount; // скидка на детский билет
+
+    // создание пустого конструктора
+    public RailwayTicketPen() {
+    }
+    // создание конструктора исходных данных детского билета (стоимость билета, количество билетов, скидка)
+    public RailwayTicketPen(float ticketPrice, int numberOfTickets, float ticketDiscount) {
+        super(ticketPrice, numberOfTickets);
+        this.ticketDiscount = ticketDiscount;
+    }
+
+    // переопределение метода подсчёта стоимости детских билетов
+    public float ticketPriceAll() {
+        return (getTicketPrice() * getNumberOfTickets() * (100 - ticketDiscount))/100; // нужно умножить количество билетов на стоимость одного билета и на скидку и всё разделить на 100
+    }
+}
